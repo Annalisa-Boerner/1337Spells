d354245e36bc7cbb9300fbf22f07ae2f58276d17
Annalisa Boerner <annalisa.boerner@gmail.com>
2023-09-14 16:57:56 -0400

    schema redesign lol

--- client/src/components/homePage.jsx
+++ client/src/components/homePage.jsx
@@ -4,8 +4,9 @@ export default function HomePage({ token, setToken }) {
      return (
           <div>
                <h2>
-                    Welcome, Level 1 Wizard! Please log in and populate your
-                    spellbook via the links above, or register below.
+                    Welcome, Level 1 Wizard! Please log in to 1337 Spells and
+                    populate your spellbook via the links above, or register
+                    below.
                     <Register token={token} setToken={setToken} />
                </h2>
           </div>
