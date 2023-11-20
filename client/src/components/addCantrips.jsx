25b82e62f963851a5eb55d4980e0b6c258bcaa79
Annalisa Boerner <annalisa.boerner@gmail.com>
2023-11-20 15:47:25 -0500

    changed params in addCantrips and addSpells

--- client/src/components/addCantrips.jsx
+++ client/src/components/addCantrips.jsx
@@ -1,73 +1,64 @@
 import { useState } from "react";
-import { createSpellbook_cantrip } from "../helpers/junction_spellbooks";
+import { createCharacter_cantrip } from "../helpers/junction_spellbooks";
 import { useNavigate } from "react-router-dom";
 
-export default function AddToSpellbooksCantrips() {
-     const [cantrip_id, setCantrip_id] = useState("");
-     const [spellbook_id, setSpellbook_id] = useState("");
-     const [cantrip_name, setCantrip_name] = useState("");
-     const navigate = useNavigate();
+export default function AddToCharactersCantrips() {
+    const [cantrip_id, setCantrip_id] = useState("");
+    //get this from localStorage instead of state
+    const [char_id, setChar_id] = useState("");
+    const [cantrip_name, setCantrip_name] = useState("");
+    const navigate = useNavigate();
 
-     async function handleSubmit(event) {
-          event.preventDefault();
-          try {
-               await createSpellbook_cantrip(
-                    cantrip_id,
-                    spellbook_id,
-                    cantrip_name
-               );
-               navigate(0);
-               navigate("/myspellbook");
-          } catch (error) {
-               alert(
-                    "There was an error adding this cantrip to your spellbook"
-               );
-          }
-     }
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try {
+            await createCharacter_cantrip(cantrip_id, char_id, cantrip_name);
+            navigate(0);
+            navigate("/myspellbook");
+        } catch (error) {
+            alert("There was an error adding this cantrip to your spellbook");
+        }
+    }
 
-     return (
-          <section id="addCantrip-container">
-               <h3>Add A Cantrip to My Spellbook</h3>
-               <form onSubmit={handleSubmit}>
-                    <input
-                         className="new-cantrip-form-bar"
-                         value={cantrip_id}
-                         type="text"
-                         name="cantrip_id"
-                         placeholder="Cantrip ID - Integer by All Cantrips Listing"
-                         onChange={(event) => setCantrip_id(event.target.value)}
-                    />
-                    <br />
+    return (
+        <section id="addCantrip-container">
+            <h3>Add A Cantrip to My Spellbook</h3>
+            <form onSubmit={handleSubmit}>
+                <input
+                    className="new-cantrip-form-bar"
+                    value={cantrip_id}
+                    type="text"
+                    name="cantrip_id"
+                    placeholder="Cantrip ID - Integer by All Cantrips Listing"
+                    onChange={(event) => setCantrip_id(event.target.value)}
+                />
+                <br />
 
-                    <input
-                         className="new-cantrip-form-bar"
-                         value={spellbook_id}
-                         type="text"
-                         name="spellbook_id"
-                         placeholder="Spellbook ID - Your Book"
-                         onChange={(event) =>
-                              setSpellbook_id(event.target.value)
-                         }
-                    />
+                <input
+                    className="new-cantrip-form-bar"
+                    value={char_id}
+                    type="text"
+                    name="char_id"
+                    placeholder="My Character ID"
+                    onChange={(event) => setChar_id(event.target.value)}
+                />
 
-                    <br />
+                <br />
 
-                    <input
-                         className="new-cantrip-form-bar"
-                         value={cantrip_name}
-                         type="text"
-                         name="cantrip_name"
-                         placeholder="Cantrip Name"
-                         onChange={(event) =>
-                              setCantrip_name(event.target.value)
-                         }
-                    />
-                    <br />
-                    <br />
-                    <button type="submit" className="add-cantrip-button">
-                         Submit
-                    </button>
-               </form>
-          </section>
-     );
+                <input
+                    className="new-cantrip-form-bar"
+                    value={cantrip_name}
+                    type="text"
+                    name="cantrip_name"
+                    placeholder="Cantrip Name"
+                    onChange={(event) => setCantrip_name(event.target.value)}
+                />
+                <br />
+                <br />
+                <button type="submit" className="add-cantrip-button">
+                    Submit
+                </button>
+            </form>
+        </section>
+    );
 }
