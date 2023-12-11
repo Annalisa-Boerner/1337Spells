2e90f31e6827d35f8b74c82965d25c1800c0354a
Annalisa Boerner <annalisa.boerner@gmail.com>
2023-12-11 11:08:21 -0500

    updated import in add spell button to remove underscore

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,14 +1,13 @@
-import Register from "./register";
+import Register from "./Register";
 
 export default function HomePage({ token, setToken }) {
-     return (
-          <div>
-               <h2>
-                    Welcome, Level 1 Wizard! Please log in to 1337 Spells and
-                    populate your spellbook via the links above, or register
-                    below.
-                    <Register token={token} setToken={setToken} />
-               </h2>
-          </div>
-     );
+    return (
+        <div>
+            <h2>
+                Welcome, Level 1 Wizard! Please log in to 1337 Spells and
+                populate your spellbook via the links above, or register below.
+                <Register token={token} setToken={setToken} />
+            </h2>
+        </div>
+    );
 }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,4 @@
 //navigation for the site
-// import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar({ token }) {
