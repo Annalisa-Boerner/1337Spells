38e5e014e70f47c454344c03e5d1a13e1f25e4c6
Annalisa Boerner <annalisa.boerner@gmail.com>
2023-12-11 11:03:29 -0500

    camelCase instead of underscores in AllCharactersCantrips.jsx

--- client/src/components/AllCharactersCantrips.jsx
+++ client/src/components/AllCharactersCantrips.jsx
@@ -1,31 +1,31 @@
 import { useState, useEffect } from "react";
 import {
-    deleteCharacter_Cantrip,
-    fetchAllCharacters_Cantrips,
+    deleteCharacterCantrip,
+    fetchAllCharactersCantrips,
 } from "../helpers/junction_spellbooks";
 import { useNavigate } from "react-router-dom";
 
 //Get all spellbooks and their cantrips
 
-export default function AllSpellbooks_Cantrips() {
-    const [allCharacters_Cantrips, setAllCharacters_Cantrips] = useState([]);
+export default function AllSpellbooksCantrips() {
+    const [allCharactersCantrips, setAllCharactersCantrips] = useState([]);
     const [searchParam, setSearchParam] = useState("");
 
     const navigate = useNavigate();
 
     useEffect(() => {
-        async function getAllCharacters_Cantrips() {
-            const spellbooks_cantrips = await fetchAllCharacters_Cantrips();
-            if (spellbooks_cantrips) {
-                setAllCharacters_Cantrips(spellbooks_cantrips);
-                return spellbooks_cantrips;
+        async function getAllCharactersCantrips() {
+            const spellbooksCantrips = await fetchAllCharactersCantrips();
+            if (spellbooksCantrips) {
+                setAllCharactersCantrips(spellbooksCantrips);
+                return spellbooksCantrips;
             } else {
                 console.error(
                     "there was an error fetching all spellbooks' cantrip content"
                 );
             }
         }
-        getAllCharacters_Cantrips();
+        getAllCharactersCantrips();
     }, []);
 
     //Delete a cantrip from a spellbook
@@ -33,7 +33,7 @@ export default function AllSpellbooks_Cantrips() {
     async function handleDelete(event) {
         event.preventDefault();
         try {
-            await deleteCharacter_Cantrip(event.target.id);
+            await deleteCharacterCantrip(event.target.id);
             navigate(0);
         } catch (error) {
             console.error(error);
@@ -43,10 +43,10 @@ export default function AllSpellbooks_Cantrips() {
     //Filter spellbooks to only show your own cantrips
 
     const spellbooksToDisplay2 = searchParam
-        ? allCharacters_Cantrips.filter((spellbook2) =>
+        ? allCharactersCantrips.filter((spellbook2) =>
               spellbook2.spellbook_id.toString().includes(searchParam)
           )
-        : allCharacters_Cantrips;
+        : allCharactersCantrips;
 
     //Render the filter bar and spellbooks/cantrips
     return (
@@ -61,20 +61,20 @@ export default function AllSpellbooks_Cantrips() {
                     onChange={(event) => setSearchParam(event.target.value)}
                 />
             </label>
-            {spellbooksToDisplay2.map((character_cantrips) => {
+            {spellbooksToDisplay2.map((characterCantrips) => {
                 return (
                     <div
                         className="flex"
-                        key={character_cantrips.characters_cantrips_id}
+                        key={characterCantrips.characters_cantrips_id}
                     >
                         <p>
-                            Spellbook ID: {character_cantrips.spellbook_id}
+                            Spellbook ID: {characterCantrips.spellbook_id}
                             <br />
-                            Cantrip: {character_cantrips.cantrip_name}
+                            Cantrip: {characterCantrips.cantrip_name}
                             <br />
                             <button
                                 onClick={handleDelete}
-                                id={character_cantrips.spellbooks_cantrips_id}
+                                id={characterCantrips.spellbooks_cantrips_id}
                             >
                                 Remove Cantrip
                             </button>
