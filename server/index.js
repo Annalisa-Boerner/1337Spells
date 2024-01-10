ed318c5625eabe8fd8b8458067b88a4998230927
Annalisa Boerner <annalisa.boerner@gmail.com>
2024-01-09 19:44:14 -0500

    added start script node dist/assets/index-5v4749a4.js

--- server/index.js
+++ server/index.js
@@ -24,15 +24,18 @@ app.use(cookieParser(COOKIE_SECRET));
 const cors = require("cors");
 app.use(cors());
 
-app.get("/", (req, res) => {
-     res.send("Hello World!");
-});
+//* is a wildcard, so it returns this at any endpoint
+app.get("*", express.static("../client/dist"));
 
 // Router: /api
 app.use("/api", require("./api"));
 
 app.listen(PORT, () => {
-     console.log(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
 
 //node index.js
+
+//
+// Static Content
+// app.use("/", express.static("./src/static"));
