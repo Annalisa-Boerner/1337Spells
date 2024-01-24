885b0f7141e0410fb284f1c891462f8bb9c32a1e
Annalisa Boerner <annalisa.boerner@gmail.com>
2024-01-24 09:31:20 -0500

    build script back to just vite build; added leet-spells/client to base url in vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/leet-spells/',
-  server: {
-    port: 8080
-  }
-})  
+    plugins: [react()],
+    base: "/leet-spells/client",
+    server: {
+        port: 8080,
+    },
+});
