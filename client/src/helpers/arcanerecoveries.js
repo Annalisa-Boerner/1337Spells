271237d095bcc38d43093bd8b5cb08686052758b
Annalisa Boerner <annalisa.boerner@gmail.com>
2024-01-26 10:40:16 -0500

    changed the base back to '/leet-spells' in config.js

--- client/src/helpers/arcanerecoveries.js
+++ client/src/helpers/arcanerecoveries.js
@@ -1,23 +1,23 @@
 const base_url = "http://localhost:8080/api";
 
 export const fetchAllArcaneRecoveries = async () => {
-     try {
-          const response = await fetch(`${base_url}/arcanerecoveries`);
-          const result = await response.json();
-          console.log(result);
-          return result;
-     } catch (error) {
-          console.error(error);
-     }
+    try {
+        const response = await fetch(`${base_url}/arcanerecoveries`);
+        const result = await response.json();
+        console.log(result);
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 export const fetchSingleArcaneRecovery = async (id) => {
-     try {
-          const response = await fetch(`${base_url}/arcanerecoveries/${id}`);
-          const result = await response.json();
-          console.log(result);
-          return result;
-     } catch (error) {
-          console.error(error);
-     }
+    try {
+        const response = await fetch(`${base_url}/arcanerecoveries/${id}`);
+        const result = await response.json();
+        console.log(result);
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
 };
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/leet-spells/client",
+    base: "/leet-spells",
     server: {
         port: 8080,
     },
