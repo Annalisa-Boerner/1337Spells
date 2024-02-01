21f5b5e3abf9f0ff2445b70745978be2d6cf7ab3
Annalisa Boerner <annalisa.boerner@gmail.com>
2024-02-01 12:28:30 -0500

    env variable to client.js

--- server/db/client.js
+++ server/db/client.js
@@ -1,19 +1,12 @@
 //we are making a postgres route
 
 const { Client } = require("pg");
-
+const db = process.env.DATABASE_URL;
 //name database by setting string to variable
 
 const spells = "1337spells";
 
 //new instance of Client with specific characterization (establish connection to db)
-const client = new Client({
-    host: "app-8013a9f3-5dbf-41e2-a704-f4f6c7c55eb5-do-user-15565143-0.c.db.ondigitalocean.com",
-    port: 25060,
-    username: db,
-    password: AVNS_MQ86iaFh7WotWEFg4OR,
-    database: db,
-    sslmode: require,
-});
+const client = new Client({ db });
 
 module.exports = client;
