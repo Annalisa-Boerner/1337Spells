e8e0d327e995530f1e37d88674a7589de4a3f18c
Annalisa Boerner <annalisa.boerner@gmail.com>
2024-02-01 11:41:10 -0500

    updated db/client.js with an object of the full connection details

--- server/db/client.js
+++ server/db/client.js
@@ -7,8 +7,13 @@ const { Client } = require("pg");
 const spells = "1337spells";
 
 //new instance of Client with specific characterization (establish connection to db)
-const client = new Client(
-    `app-8013a9f3-5dbf-41e2-a704-f4f6c7c55eb5-do-user-15565143-0.c.db.ondigitalocean.com`
-);
+const client = new Client({
+    host: "app-8013a9f3-5dbf-41e2-a704-f4f6c7c55eb5-do-user-15565143-0.c.db.ondigitalocean.com",
+    port: 25060,
+    username: db,
+    password: AVNS_MQ86iaFh7WotWEFg4OR,
+    database: db,
+    sslmode: require,
+});
 
 module.exports = client;
