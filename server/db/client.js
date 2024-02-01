522e8d6f272715bb93b5bf9bbeeed4e58ff69191
Annalisa Boerner <annalisa.boerner@gmail.com>
2024-02-01 11:36:19 -0500

    updated db/client.js with address of database

--- server/db/client.js
+++ server/db/client.js
@@ -7,6 +7,8 @@ const { Client } = require("pg");
 const spells = "1337spells";
 
 //new instance of Client with specific characterization (establish connection to db)
-const client = new Client(`http://localhost:5432/${spells}`);
+const client = new Client(
+    `app-8013a9f3-5dbf-41e2-a704-f4f6c7c55eb5-do-user-15565143-0.c.db.ondigitalocean.com`
+);
 
 module.exports = client;
