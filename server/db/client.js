cf3ef5a4f5069b935a0a9e132707559dea45e96c
Annalisa Boerner <annalisa.boerner@gmail.com>
2024-02-07 21:32:22 -0500

    commented out client.js in order to try to deploy backend without db connection :

--- server/db/client.js
+++ server/db/client.js
@@ -1,12 +1,12 @@
-//we are making a postgres route
+// //we are making a postgres route
 
-const { Client } = require("pg");
-const db = process.env.DATABASE_URL;
-//name database by setting string to variable
+// const { Client } = require("pg");
+// const db = process.env.DATABASE_URL;
+// //name database by setting string to variable
 
-const spells = "1337spells";
+// const spells = "1337spells";
 
-//new instance of Client with specific characterization (establish connection to db)
-const client = new Client({ db });
+// //new instance of Client with specific characterization (establish connection to db)
+// const client = new Client({ db });
 
-module.exports = client;
+// module.exports = client;
