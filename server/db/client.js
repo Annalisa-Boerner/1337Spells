911786dd45b21795c276e16715a7f4508733bd49
Annalisa Boerner <annalisa.boerner@gmail.com>
2024-02-09 15:16:17 -0500

    env variables defined in client.js

--- server/db/client.js
+++ server/db/client.js
@@ -2,6 +2,7 @@
 
 const { Client } = require("pg");
 const db = process.env.DATABASE_URL;
+const password = process.env.DATABASE_URL;
 
 //name database by setting string to variable
 
@@ -11,7 +12,7 @@ const spells = "1337spells";
 const client = new Client({
     user: "db",
     host: "app-8013a9f3-5dbf-41e2-a704-f4f6c7c55eb5-do-user-15565143-0.c.db.ondigitalocean.com",
-    database: "db",
+    database: db,
     password: password,
     port: 8080,
     ssl: {
