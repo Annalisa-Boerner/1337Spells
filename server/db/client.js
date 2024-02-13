2053e433a28441b86481482a7669ccee09aed818
Annalisa Boerner <annalisa.boerner@gmail.com>
2024-02-13 10:36:45 -0500

    updates to client.js db dtails

--- server/db/client.js
+++ server/db/client.js
@@ -1,7 +1,7 @@
 //we are making a postgres route
 
 const { Client } = require("pg");
-const db = process.env.DATABASE_URL;
+// const db = process.env.DATABASE_URL;
 const password = process.env.DATABASE_URL;
 
 //name database by setting string to variable
@@ -10,32 +10,15 @@ const spells = "1337spells";
 
 //new instance of Client with specific characterization (establish connection to db)
 const client = new Client({
-    db
+    user: "doadmin",
+    host: "app-8013a9f3-5dbf-41e2-a704-f4f6c7c55eb5-do-user-15565143-0.c.db.ondigitalocean.com",
+    database: "db",
+    password: password,
+    port: 25060,
+    ssl: {
+        rejectUnauthorized: false, // Required because DigitalOcean SSL is self-signed
+    },
 });
 
 module.exports = client;
-// const { Pool } = require("pg");
-// const password = process.env.PASSWORD;
 
-// const pool = new Pool({
-//     user: "db",
-//     host: "app-8013a9f3-5dbf-41e2-a704-f4f6c7c55eb5-do-user-15565143-0.c.db.ondigitalocean.com",
-//     database: "db",
-//     password: password,
-//     port: 8080,
-//     ssl: {
-//         rejectUnauthorized: false, // Required because DigitalOcean SSL is self-signed
-//     },
-// });
-
-// // Test the connection
-// pool.query("SELECT NOW()", (err, res) => {
-//     if (err) {
-//         console.error("Error connecting to PostgreSQL database:", err.stack);
-//     } else {
-//         console.log("Connected to PostgreSQL database at:", res.rows[0].now);
-//     }
-// });
-
-// // Export the pool so it can be used by other modules
-// module.exports = { pool };
