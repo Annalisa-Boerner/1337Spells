73d015f66e1ab4b0f1c7eb86ec7b69223db95eeb
Annalisa Boerner <annalisa.boerner@gmail.com>
2024-02-14 11:26:40 -0500

    db

--- server/db/client.js
+++ server/db/client.js
@@ -10,7 +10,7 @@ const spells = "1337spells";
 
 //new instance of Client with specific characterization (establish connection to db)
 const client = new Client({
-    user: "doadmin",
+    user: "db",
     host: "app-8013a9f3-5dbf-41e2-a704-f4f6c7c55eb5-do-user-15565143-0.c.db.ondigitalocean.com",
     database: "db",
     password: password,
