0748d7de51d0b3da958437f1a41b642827fd506a
Annalisa Boerner <annalisa.boerner@gmail.com>
2024-03-08 16:31:16 -0500

    added register message logic above return

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,27 +1,64 @@
 import { useState } from "react";
 import { registerCharacter } from "../fetching";
 import { useNavigate } from "react-router-dom";
+import { fetchAllCharacters } from "../helpers/characters";
 
 export default function Register({ token, setToken }) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [name, setName] = useState("");
+    const [successMessage, setSuccessmessage] = useState(null);
+    const [error, setError] = useState(null);
+
+    //Putting display message conditions on state
+    const [characterExists, setCharacterExists] = useState(false);
+    const [nameTooShort, setNameTooShort] = useState(false);
+    const [passwordTooShort, setPasswordTooShort] = useState(false);
+
     const nav = useNavigate();
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        console.log("handle submit variables", username, password, name);
-        const register = await registerCharacter(username, password, name);
-        console.log("register in the Register handleSubmit ", register);
-        setToken(register.token);
 
-        setUsername("");
-        setPassword("");
-        setName("");
-        if (register) {
-            nav("/login");
-        } else {
-            alert("registration probz");
+        //Fetch all characters to see if character's username already exsits
+
+        try {
+            const characterResponse = await fetchAllCharacters();
+            if (password.length <= 8) {
+                setPasswordTooShort(true);
+            } else if (
+                characterResponse.filter(
+                    (character) => character.username === username
+                ).length > 0
+            ) {
+                setCharacterExists(true);
+            } else if (username.length <= 4) {
+                setNameTooShort(true);
+            } else {
+                console.log(
+                    "handle submit variables",
+                    username,
+                    password,
+                    name
+                );
+                const register = await registerCharacter(
+                    username,
+                    password,
+                    name
+                );
+                console.log("register in the Register handleSubmit ", register);
+                setToken(register.token);
+                setUsername("");
+                setPassword("");
+                setName("");
+                if (register) {
+                    nav("/login");
+                } else {
+                    alert("registration probz");
+                }
+            }
+        } catch (error) {
+            setError(error.message);
         }
     };
 
